c46abc3e24a4e1f90820e6a29e1a503260f6dd51
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2024-02-16 10:04:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 console.clear();
 
-const elFoodsReduce = document.querySelectorAll(".reduce-weight .food");
-const elFoodsGain = document.querySelectorAll(".gain-weight .food");
+const elShoesReduce = document.querySelectorAll(".reduce-weight .shoe");
+const elShoesGain = document.querySelectorAll(".gain-weight .shoe");
 
-elFoodsReduce.forEach((food) => {
-  food.addEventListener("click", () => {
-    elFoodsReduce.forEach((el) => delete el.dataset.active);
-    elFoodsGain.forEach((el) => delete el.dataset.active);
-    food.dataset.active = true;
+elShoesReduce.forEach((shoe) => {
+  shoe.addEventListener("click", () => {
+    elShoesReduce.forEach((el) => delete el.dataset.active);
+    elShoesGain.forEach((el) => delete el.dataset.active);
+    shoe.dataset.active = true;
   });
 });
 
-elFoodsGain.forEach((food) => {
-  food.addEventListener("click", () => {
-    elFoodsGain.forEach((el) => delete el.dataset.active);
-    elFoodsReduce.forEach((el) => delete el.dataset.active);
-    food.dataset.active = true;
+elShoesGain.forEach((shoe) => {
+  shoe.addEventListener("click", () => {
+    elShoesGain.forEach((el) => delete el.dataset.active);
+    elShoesReduce.forEach((el) => delete el.dataset.active);
+    shoe.dataset.active = true;
   });
 });
+
